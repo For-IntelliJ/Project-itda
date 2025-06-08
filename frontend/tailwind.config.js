d499e459f8d4406f05cc936041b1a88939e0ba93
God-Da <150757592+God-Da@users.noreply.github.com>
2025-06-09 06:20:14 +0900

    tailwinds 설치

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // ✅ React 프로젝트용
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffffff',
+        hover: '#3D4EFE',
+        font: '#37474F',
+        line: '#777777',
+        warning: '#E44B4B',
+        kakao: '#FEE500',
+        naver: '#03C75A',
+        errorRed: '#FF3333',
+      },
+      fontFamily: {
+        sans: ['Noto Sans KR', 'sans-serif'],
+        pretendard: ['Pretendard', 'sans-serif'],
+        dnf: ['DNFBitBit2', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
