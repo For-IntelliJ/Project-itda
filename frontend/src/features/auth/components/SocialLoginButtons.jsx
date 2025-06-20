f24ddff99784cca18cebae564787ab6b382a15ce
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-06-20 23:02:30 +0900

    카카오 로그인 버튼 HTTP 이동
    기본 SNS로그인 버튼에 카카오 인증 페이지로 이동하게 하는 URL 함수를 만들어 onClick={여기} 에 삽입해줍니다.

--- frontend/src/features/auth/components/SocialLoginButtons.jsx
+++ frontend/src/features/auth/components/SocialLoginButtons.jsx
@@ -1,14 +1,30 @@
-//카카오 로그인 컴포넌트~ 회원가입/ 로그인에 쓰기
+// SocialLoginButtons.jsx
 import React from 'react';
 
-const SocialLoginButtons = () => (
-    <button className="mx-auto flex h-auto w-auto flex-col items-center border-none bg-transparent p-0">
-        <img
-            src={process.env.PUBLIC_URL + '/img/kakao_login.png'}
-            alt="카카오 로그인"
-            className="block"
-        />
-    </button>
-);
+const SocialLoginButtons = () => {
+    const REST_API_KEY = "a680d77f4256f7e8110b6bfb85250623";
+    const REDIRECT_URI = "http://localhost:8080/auth/kakao/callback";
+
+    const handleLogin = () => {
+        const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+        console.log("📦 카카오 로그인 URL:", KAKAO_URL);
+        console.log("카카오 버튼 클릭됨")
+        window.location.href =
+            `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    };
+
+    return (
+        <button
+            onClick={handleLogin}
+            className="mx-auto flex h-auto w-auto flex-col items-center border-none bg-transparent p-0"
+        >
+            <img
+                src={process.env.PUBLIC_URL + '/img/kakao_login.png'}
+                alt="카카오 로그인"
+                className="block"
+            />
+        </button>
+    );
+};
 
 export default SocialLoginButtons;
